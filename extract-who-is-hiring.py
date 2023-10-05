dc3d6bce47424108b1d9ca49b6a5535b8c37d425
Hugh Brown <hughdbrown@yahoo.com>
2023-10-05 12:50:51 -0600

    Add summarizing search hits

--- extract-who-is-hiring.py
+++ extract-who-is-hiring.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from collections import defaultdict
 import json
 from pathlib import Path
 from pprint import pprint
@@ -14,6 +15,7 @@ DATE_RE = re.compile(r'''(\d{4}-\d{2}-\d{2})''')
 @click.command()
 @click.option('-c', '--company', required=False, type=str)
 def main(company=None):
+    d = defaultdict(set)
     if company:
         print(f"Searching for '{company}'")
         company_re = re.compile(fr'''\b{company}\b''', re.IGNORECASE)
@@ -27,13 +29,19 @@ def main(company=None):
                 if text is not None:
                     soup = BeautifulSoup(text, "lxml")
                     text = " ".join(soup.find_all(text=True))
-                    if RUST_RE.search(text):
+                    if (m := RUST_RE.search(text)):
                         if company:
                             if company_re.search(text):
                                 print(text)
                         else:
                             first_line = text.splitlines()[0]
                             print(f"{i} {first_line[:100]}")
+                        match = m.group(1)
+                        d[match].add(str(p))
+
+    #for k, v in sorted(d.items()):
+    #    for p in sorted(v):
+    #        print(f"{k} {p}")
 
 
 if __name__ == '__main__':
