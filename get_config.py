04ea45a9ed8c74bc841d012322b060739ab8ed79
Hugh Brown <hughdbrown@yahoo.com>
2024-08-05 10:10:54 -0600

    Change data directory from home root

--- get_config.py
+++ get_config.py
@@ -3,4 +3,4 @@ from pathlib import Path
 
 def cache_dir() -> Path:
     """Get the location to cache data for Who Is Hiring"""
-    return Path().home()
+    return Path().home().joinpath("scratch-data")
